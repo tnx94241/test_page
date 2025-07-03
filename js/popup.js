126772a4dd0ee061ad6152e6bf62220a0ced5ab5
Ayumi Nakamichi <ayumi.nakamichi.kf@renesas.com>
2025-07-03 20:03:03 +0900

    fix index.html and js/popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const popupOverlay = document.getElementById('image-popup-overlay');
   const popupImage = document.getElementById('image-popup');
-  const images = document.querySelectorAll('.popup-image');
 
+  // 画像からポップアップ
+  const images = document.querySelectorAll('.popup-image');
   images.forEach(img => {
     img.addEventListener('click', () => {
       popupImage.src = img.src;
@@ -11,6 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // 背景（オーバーレイ）をクリックしたときだけ閉じる
+  popupOverlay.addEventListener('click', (e) => {
+    if (e.target === popupOverlay) {
+      popupOverlay.style.display = 'none';
+      popupImage.src = '';
+    }
+  });
+
+  // 文字リンクからポップアップ
+  const triggers = document.querySelectorAll('.popup-trigger');
+  triggers.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault(); // リンク移動を止める
+      const imageUrl = link.dataset.image;
+      popupImage.src = imageUrl;
+      popupOverlay.style.display = 'flex';
+    });
+  });
+
+  // 画像の外側をクリックで閉じる
   popupOverlay.addEventListener('click', (e) => {
     if (e.target === popupOverlay) {
       popupOverlay.style.display = 'none';
